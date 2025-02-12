cd24e19c14926d26bb8477fb60646311fa83b84e
sfiorilla <sfiorilla01@gmail.com>
2025-02-12 13:07:04 +0100

    committing track eval

--- README.md
+++
@@ -1,3 +0,0 @@
-# ReTrack: Reidentification and Tracking
-The inpact of Person Re-Identification models into tracks algorithm.
-
---
+++ run_experiments.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Hi, The test Starts!"
+cd ./BoT-SORT
+echo "Current directory: $(pwd)"
+rm -f ./TrackEval/data/trackers/mot_challenge/MOT17-train/last_execution/data/*
+list=("02" "04" "05" "09" "10" "11" "13")
+for i in "${list[@]}"
+do
+    echo "Running Seq$i"
+    python3 tools/demo.py image --path ./datasets/MOT17/train/MOT17-"$i"-DPM/img1 -f yolox/exps/example/mot/yolox_x_mix_det.py -c pretrained/bytetrack_x_mot17.pth.tar --with-reid --fuse-score --fp16 --fuse --save_result
+done
+cd ../TrackEval/
+echo "Metrics evaluation"
+python scripts/run_mot_challenge.py --BENCHMARK MOT17 --SPLIT_TO_EVAL train --TRACKERS_TO_EVAL last_execution --METRICS HOTA CLEAR Identity VACE --USE_PARALLEL False --NUM_PARALLEL_CORES 1  --PLOT_CURVES False
+echo "The test is completed."
